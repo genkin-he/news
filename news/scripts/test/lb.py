54c663f4f4eceb9c5a526154b4f09a16d1375887
genkin-he <mengzhi.he@longbridge.sg>
2024-10-18 11:39:28 +0800

    add lb

--- news/scripts/test/lb.py
+++ news/scripts/test/lb.py
@@ -1,4 +1,11 @@
 from longport.openapi import HttpClient
 http_cli = HttpClient.from_env()
 resp = http_cli.request("get", "/v1/content/articles?slug=stock_news")
-print(resp)
+print("article list: \n",resp)
+
+# 翻页, 翻页参数是上一次请求的 next_params
+resp = http_cli.request("get", "/v1/content/articles?slug=stock_news&params=eyJ0YWlsX21hcmsiOjE3MjkyMTk4MjMwMDAwMDAsImxpbWl0IjoyMCwic29ydCI6IkRFU0MifQ")
+print("article list2: \n",resp)
+
+resp = http_cli.request("get", "/v1/content/detail?target_id=217179400&target_type=Post")
+print("article detail: \n", resp)
